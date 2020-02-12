77379a538abb4d06a0efb1eb425be5e0fe98d47a
rtodd001 <rtodd001@ucr.edu>
2020-02-11 18:33:09 -0800

    Added functionality to switch between states (Daily, Social/Study, Seminar/Workshop). Still need to implement the back end changes in the sheets

--- CardRunnerInfinite.py
+++ CardRunnerInfinite.py
@@ -7,25 +7,53 @@ cardUser = ''
 new_title = sheet_title(sheet.row_values(1))
 currentTitle = sheet.row_values(1)
 
+# Unique messages to identify the state of the swipe
+dailyMess = "Hello! Please Swipe Your Card to sign in: \n"
+studySocailMess = "Study Jam sign in! Please Swipe Your Card to sign in: \n"
+seminarMess = "Welcome to the Seminar! Please Swipe Your Card to sign in: \n"
 
 
 def main():
     if new_title != currentTitle:
                 sheet.delete_row(1)
                 sheet.insert_row(new_title,1)
+    
+    # Keep track of the current state
+    # These variables will dictate the population of the Google Sheet
+    # d : Daily/Regular days
+    # s : Social/Study Jams 
+    # w : Seminar/Workshops
+    #
+    # Default is Daily/Regular days
+    state = 'd'
 
     while True:
         clear()
-        cardUser= getpass("Hello! Please Swipe Your Card to sign in: \n")
-        # cardUser= getpass("Hello! Please Swipe Your Card to sign in: ")
+
+
+
+        # Print out the corresponding message for the state
+        if(state ==  "d"):
+            cardUser = getpass("Hello! Please Swipe Your Card to sign in: \n")
+        elif(state == "s"):
+            cardUser = getpass("Study Jam sign in! Please Swipe Your Card to sign in: \n")
+        elif(state == "w"):
+            cardUser = getpass("Welcome to the Seminar! Please Swipe Your Card to sign in: \n")
+        
+        temp = cardUser.lower()
 
         #Invalid swipe
         if(cardUser == '' or not cardUser):
             print("Invalid Swipe..... Please Swipe again")
             clear()
         
+        # Change the state based on input
+        elif(temp == "d" or temp == "s" or temp == "w"):
+            state = cardUser.lower()
+            completeData = False
+
         #Manual Entry
-        elif(cardUser.lower() == "m"):
+        elif(temp == "m"):
             firstName=''
             lastName=''
             sid =''

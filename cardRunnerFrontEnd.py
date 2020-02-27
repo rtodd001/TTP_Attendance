d649acd0cdd706473261ffe39cd0444f44bcd037
Sam Tapia <stapi010@ucr.edu>
2020-02-27 15:22:50 -0800

    center app to screen

--- cardRunnerFrontEnd.py
+++ cardRunnerFrontEnd.py
@@ -1,17 +1,23 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QLineEdit, QFormLayout
+from PyQt5.QtWidgets import QMainWindow, QDesktopWidget, QGridLayout
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
+from PyQt5 import QtGui
 
 def window():
    app = QApplication(sys.argv)
    window = QWidget() #running window acts like a container 
    layout = QVBoxLayout() #stacks widgets vertically make
    formLayout = QFormLayout() #2 column layout left is string val and right is field widget
+   
+   #fields
    firstName = QLineEdit()
    lastName = QLineEdit()
    idCard = QLineEdit()
    idCard.setEchoMode(QLineEdit.Password)
+   
+   #center window
    
 
    textLabel = QLabel(window)
@@ -27,9 +33,34 @@ def window():
    # layout.addChildLayout(QFormLayout(formLayout))
    layout.addWidget(QPushButton("Submit"))   
 
-   window.setGeometry(50,50,320,200)
-   window.setWindowTitle("TTP Card Scanner")
    # window.setLayout(layout)
    window.setLayout(formLayout)
+   centerWindow(window)
    window.show()
    sys.exit(app.exec_())
+
+def centerWindow(ui):
+   ui.setGeometry(0,0,400,600)
+   ui.setWindowTitle("TTP Card Scanner")
+   qr = ui.frameGeometry()
+   cp = QDesktopWidget().availableGeometry().center()
+   qr.moveCenter(cp)
+   ui.move(qr.topLeft())
+   ui.setWindowIcon(QtGui.QIcon('./img/ttpLogo.png'))
+
+#object class
+class Window(QtGui.QMainWindow):
+
+   def __init__(self):
+      super(Window, self).__init__()
+      self.setGeometry(0,0,400,600)
+      self.setWindowTitle("TTP Card Scanner")
+      self.setWindowIcon(QtGui.QIcon('./img/ttpLogo.png'))
+      qr = self.frameGeometry()
+      cp = QDesktopWidget().availableGeometry().center()
+      qr.moveCenter(cp)
+      self.move(qr.topLeft())
+      self.home()
+
+   def home(self):
+
